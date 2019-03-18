777cf3682686e5a6b00b0e1c34005ff1d25377c1
Kai Tödter <kai.toedter@siemens.com>
2019-03-18 12:52:08 +0100

    fix: fixed port mapping for heroku deployment

--- backend/src/main/java/com/toedter/movies/MoviesDemoApplication.java
+++ backend/src/main/java/com/toedter/movies/MoviesDemoApplication.java
@@ -7,6 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class MoviesDemoApplication extends WebMvcConfigurerAdapter {
     public static void main(String... args) {
+        // when deployed as a docker container to Heroku
+        // Heroku sets the PORT environment variable
+        // The DYNO environment variable is just to make sure to run in an Heroku environment
+        String herokuPort = System.getenv().get("PORT");
+        String herokuDyno = System.getenv().get("DYNO");
+        if (herokuPort != null && herokuDyno != null) {
+            System.getProperties().put("server.port", herokuPort);
+        }
+
         SpringApplication.run(MoviesDemoApplication.class, args);
     }
 }
