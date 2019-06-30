f06c90d82eece1fed0a6c9534061d1fe48fd7002
Kai Toedter <kai@toedter.com>
2019-06-30 08:50:40 +0200

    fix: fixed root API links
    Now movies and directors links are rendered as object rather than array

--- backend/src/main/java/com/toedter/movies/HypermediaConfiguration.java
+++ backend/src/main/java/com/toedter/movies/HypermediaConfiguration.java
@@ -1,24 +1,22 @@
 package com.toedter.movies;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.hateoas.mediatype.hal.HalConfiguration;
 
 import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL_FORMS;
 
 @Configuration
 // @EnableHypermediaSupport(type = { HAL, HAL_FORMS, UBER, COLLECTION_JSON })
-@EnableHypermediaSupport(type = { HAL_FORMS })
+@EnableHypermediaSupport(type = {HAL_FORMS})
 class HypermediaConfiguration {
-    @Bean
-    public HalConfiguration linkRelationBasedPolicy() {
-        return new HalConfiguration()
-                .withRenderSingleLinksFor(
-                        LinkRelation.of("directors"), HalConfiguration.RenderSingleLinks.AS_ARRAY)
-                .withRenderSingleLinksFor(
-                        LinkRelation.of("movies"), HalConfiguration.RenderSingleLinks.AS_ARRAY);
-    }
-
+//    This is the config if we always want to render movies and directors as array
+//
+//    @Bean
+//    public HalFormsConfiguration linkRelationBasedPolicy() {
+//        return new HalFormsConfiguration()
+//                .getHalConfiguration().withRenderSingleLinksFor(
+//                        LinkRelation.of("directors"), HalConfiguration.RenderSingleLinks.AS_ARRAY)
+//                .withRenderSingleLinksFor(
+//                        LinkRelation.of("movies"), HalConfiguration.RenderSingleLinks.AS_ARRAY);
+//    }
 }
