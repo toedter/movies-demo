f7674ad7e258f8a0aff8400f2d2dc01c23cb980c
Kai Toedter <kai@toedter.com>
2020-10-06 14:25:55 +0200

    fix: fix base for paging links

--- backend/src/main/java/com/toedter/movies/director/DirectorController.java
+++ backend/src/main/java/com/toedter/movies/director/DirectorController.java
@@ -43,32 +43,33 @@ public class DirectorController {
                 .collect(Collectors.toList());
 
         Link selfLink = linkTo(DirectorController.class).slash("directors?page=" + page + "&size=" + size).withSelfRel();
+        Link baseLink = linkTo(DirectorController.class).slash("directors").withSelfRel();
 
         PagedModel.PageMetadata pageMetadata =
                 new PagedModel.PageMetadata(pagedResult.getSize(), pagedResult.getNumber(), pagedResult.getTotalElements(), pagedResult.getTotalPages());
         final PagedModel<DirectorRepresentationModel> pagedModel =
-                new PagedModel<>(directorResources, pageMetadata);
+                PagedModel.of(directorResources, pageMetadata);
 
 
         final Pageable prev = pageRequest.previous();
         if (prev.getPageNumber() < page) {
-            Link prevLink = Link.of(selfLink.getHref() + "?page=" + prev.getPageNumber() + "&size=" + prev.getPageSize()).withRel(IanaLinkRelations.PREV);
+            Link prevLink = Link.of(baseLink.getHref() + "?page=" + prev.getPageNumber() + "&size=" + prev.getPageSize()).withRel(IanaLinkRelations.PREV);
             pagedModel.add(prevLink);
         }
 
         final Pageable next = pageRequest.next();
         if (next.getPageNumber() > page && next.getPageNumber() < pagedResult.getTotalPages()) {
-            Link nextLink = Link.of(selfLink.getHref() + "?page=" + next.getPageNumber() + "&size=" + next.getPageSize()).withRel(IanaLinkRelations.NEXT);
+            Link nextLink = Link.of(baseLink.getHref() + "?page=" + next.getPageNumber() + "&size=" + next.getPageSize()).withRel(IanaLinkRelations.NEXT);
             pagedModel.add(nextLink);
         }
 
         if (page > 0) {
-            Link firstLink = Link.of(selfLink.getHref() + "?page=0&size=" + size).withRel(IanaLinkRelations.FIRST);
+            Link firstLink = Link.of(baseLink.getHref() + "?page=0&size=" + size).withRel(IanaLinkRelations.FIRST);
             pagedModel.add(firstLink);
         }
 
         if (page < pagedResult.getTotalPages() - 1) {
-            Link lastLink = Link.of(selfLink.getHref() + "?page=" + (pagedResult.getTotalPages() - 1) + "&size=" + size).withRel(IanaLinkRelations.LAST);
+            Link lastLink = Link.of(baseLink.getHref() + "?page=" + (pagedResult.getTotalPages() - 1) + "&size=" + size).withRel(IanaLinkRelations.LAST);
             pagedModel.add(lastLink);
         }
 
@@ -78,7 +79,7 @@ public class DirectorController {
     @GetMapping("/directors/{id}")
     public ResponseEntity<DirectorRepresentationModel> findOne(@PathVariable Long id) {
         return repository.findById(id)
-                .map(director -> new DirectorRepresentationModel(director))
+                .map(DirectorRepresentationModel::new)
                 .map(ResponseEntity::ok) //
                 .orElse(ResponseEntity.notFound().build());
     }
--- backend/src/main/java/com/toedter/movies/movie/MovieController.java
+++ backend/src/main/java/com/toedter/movies/movie/MovieController.java
@@ -48,7 +48,7 @@ public class MovieController {
                 .collect(Collectors.toList());
 
         Link selfLink = linkTo(MovieController.class).slash("movies?page=" + page + "&size=" + size).withSelfRel();
-
+        Link baseLink = linkTo(MovieController.class).slash("movies").withSelfRel();
         final Affordance newMovieAffordance =
                 afford(methodOn(MovieController.class).newMovie(null));
 
@@ -59,23 +59,23 @@ public class MovieController {
 
         final Pageable prev = pageRequest.previous();
         if (prev.getPageNumber() < page) {
-            Link prevLink = Link.of(selfLink.getHref() + "?page=" + prev.getPageNumber() + "&size=" + prev.getPageSize()).withRel(IanaLinkRelations.PREV);
+            Link prevLink = Link.of(baseLink.getHref() + "?page=" + prev.getPageNumber() + "&size=" + prev.getPageSize()).withRel(IanaLinkRelations.PREV);
             entityModels.add(prevLink);
         }
 
         final Pageable next = pageRequest.next();
         if (next.getPageNumber() > page && next.getPageNumber() < pagedResult.getTotalPages()) {
-            Link nextLink = Link.of(selfLink.getHref() + "?page=" + next.getPageNumber() + "&size=" + next.getPageSize()).withRel(IanaLinkRelations.NEXT);
+            Link nextLink = Link.of(baseLink.getHref() + "?page=" + next.getPageNumber() + "&size=" + next.getPageSize()).withRel(IanaLinkRelations.NEXT);
             entityModels.add(nextLink);
         }
 
         if (page > 0) {
-            Link firstLink = Link.of(selfLink.getHref() + "?page=0&size=" + size).withRel(IanaLinkRelations.FIRST);
+            Link firstLink = Link.of(baseLink.getHref() + "?page=0&size=" + size).withRel(IanaLinkRelations.FIRST);
             entityModels.add(firstLink);
         }
 
         if (page < pagedResult.getTotalPages() - 1) {
-            Link lastLink = Link.of(selfLink.getHref() + "?page=" + (pagedResult.getTotalPages() - 1) + "&size=" + size).withRel(IanaLinkRelations.LAST);
+            Link lastLink = Link.of(baseLink.getHref() + "?page=" + (pagedResult.getTotalPages() - 1) + "&size=" + size).withRel(IanaLinkRelations.LAST);
             entityModels.add(lastLink);
         }
 
