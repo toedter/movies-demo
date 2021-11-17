4558c75822f9107dcce76a24c8416e8e55e990ca
Kai Toedter <kai@toedter.com>
2021-11-17 08:44:16 +0100

    feat(backend): add CORS filter

---
+++ backend/src/main/java/com/toedter/movies/SimpleCORSFilter.java
@@ -0,0 +1,26 @@
+package com.toedter.movies;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Expose-Headers", "Allow,Location,Date,Link,ETag,X-Application-Context");
+        chain.doFilter(req, res);
+	}
+
+	public void init(FilterConfig filterConfig) {}
+
+	public void destroy() {}
+
+}
